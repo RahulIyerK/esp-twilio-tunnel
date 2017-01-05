62e236992b13ef052f8311ace368d097637a7863
RahulIyerK <rahul.iyer2@gmail.com>
2017-01-05 11:25:51 -0800

    cleaned up code

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-
 <?php
-  //espttpwd
 
-  require 'vendor/autoload.php';
+  require 'vendor/autoload.php'; //composer autoload twilio API
 
+  //instructions
   echo ("<h2> ESP Twilio Tunnel Accepts HTTP GET requests from the ESP8266 module </h2>");
   echo ("<br>");
   echo ("<h3> The GET parameters are as follows: </h3>");
@@ -14,22 +13,22 @@
   echo ("<p> <i> message </i> : Message to send </p>");
   echo ("<br>");
 
-  //esp8266 twilio "tunnel"
-  //accepts HTTP communications from an ESP8266 module and makes appropriate HTTPS connections to the Twilio MMS API
-  //receive esp GET data
-
+  //GET parameters
   $account_sid = $_GET["account_sid"]; //user's twilio account sid
   $auth_token = $_GET["auth_token"]; //user's twilio auth token
   $twilio_number = $_GET["twilio_number"]; //user's twilio number
   $destination_number = $_GET["destination_number"];
   $message = $_GET["message"]; //message to send over MMS
 
-  if ((strlen($account_sid) > 0)&&(strlen($auth_token) > 0)&&(strlen($twilio_number) > 0)&&(strlen($destination_number) > 0)&&(strlen($message) > 0))
+  //check if GET fields are are all not empty
+  if ((strlen($account_sid) > 0) && (strlen($auth_token) > 0) && (strlen($twilio_number) > 0)
+    && (strlen($destination_number) > 0) && (strlen($message) > 0))
   {
-    //send to twilio mms api
+    //test 
+    //file_put_contents('message_log.html', "<p>". date("Y-m-d H:i:s") .
+    //  " , " . $account_sid . " , " . $auth_token . " , " . $twilio_number . " , " . $destination_number . " , " . $message . "</p>");
 
-    //test
-    //file_put_contents('message_log.html', "<p>". date("Y-m-d H:i:s") . " , " . $account_sid . " , " . $auth_token . " , " . $twilio_number . " , " . $destination_number . " , " . $message . "</p>");
+    //send to twilio mms api
 
     $client = new Services_Twilio($account_sid, $auth_token);
     $client->account->messages->create(array(
